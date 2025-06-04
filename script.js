75be7dd9189facb359778ed840e6aae724c5780b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-06-03 22:14:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ boy: b
 			.replace(/"(.+?)"/g, "\x1b[36m\"$1\"\x1b[0m")
 			.replace(new RegExp(`((?:${g.name}|${b.name}|${g.species}|${b.species})[^ ]*)`, "gi"), "\x1b[33m\x1b[4m$1\x1b[0m")
 			.replace(/\s+\n/g, "\n"),
-`\n# ${title}\n## ${plot}`
+`\n${"*".repeat(50)}\n# ${title}\n## Plot\n${plot}`
 		);
 	};
 };

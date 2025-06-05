4aae3ac050c8c7325a58b3cbe35f1992e2ef8e48
JactusTheCactus <threateninglyamusing@gmail.com>
2025-06-04 22:07:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -58,8 +58,7 @@ class Story {
 		[this.girl, this.boy].forEach(char => {
 			if (char) {
 				if (!char.name) {
-					char.name = "Name",
-char.nameFirst = "Name"[0]
+					char.name = "Name"
 				};
 				if (!char.species) {
 					char.species = "Species"
@@ -67,10 +66,10 @@ char.nameFirst = "Name"[0]
 			} else {
 				char = {
 					name: "Name",
-nameFirst: "Name"[0],
 					species: "Species"
 				};
 			};
+char.nameFirst = "Name"[0]
 		});
 		this.plot = "";
 		this.notes = "";

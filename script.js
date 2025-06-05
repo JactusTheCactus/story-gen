bd1d078b4bbf439c11b7b120471a4f39caf6851a
JactusTheCactus <threateninglyamusing@gmail.com>
2025-06-04 22:28:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ ${notes}${"●".repeat(50)}`
 			.replace(/__(.+?)__/g, "\x1b[31m\x1b[1m$1\x1b[0m")
 			.replace(/_(.+?)_/g, "\x1b[32m\x1b[3m$1\x1b[0m")
 			.replace(/"(.+?)"/g, "\x1b[36m\"$1\"\x1b[0m")
-			.replace(new RegExp(`((?:${g.name}|${b.name}|${g.species}|${b.species})[^ ,]*)`, "gi"), "\x1b[33m\x1b[4m$1\x1b[0m")
+			.replace(new RegExp(`((?:${g.name}|${b.name}|${g.species}|${b.species})[\w']*)`, "gi"), "\x1b[33m\x1b[4m$1\x1b[0m")
 			.replace(/\s+\n/g, "\n")
 		);
 	};

14767537861c75ab1264c666952ecafdd7c0c96a
JactusTheCactus <threateninglyamusing@gmail.com>
2025-06-17 22:57:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -258,7 +258,7 @@ ${`${section === "title" ? "=" : "-"
 			.replace(/\^(.*)\^/g, "<sup>$1</sup>")
 			.replace(/~(.*)~/g, "<sub>$1</sub>")
 .replace(/\t/g," ".repeat(4))
-[fnA, fnB] = getFootnotes(this.output)
+const [fnA, fnB] = getFootnotes(this.output)
 this.output = `
 ${fnA}
 ***
